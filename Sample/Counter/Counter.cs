3f3bd837b7e99da9d7c10c0bd93add06a7457a5c
fireskyvvv <fireskyvvv@gmail.com>
2025-05-25 23:11:19 +0900

    add testing package

---
+++ Sample/Counter/Counter.cs
@@ -0,0 +1,16 @@
+using Moon.Runtime.Core;
+using UnityEngine.UIElements;
+//using ViewInstaller = Moon.Sample.Generated.ViewInstaller;
+
+namespace Moon.Sample.Counter
+{
+    /*[MoonView(ViewKey = nameof(CounterView))]*/
+    [MoonView(ReferenceMode = MoonViewAttribute.ViewReferenceMode.AssetBundle)]
+    public partial class Counter : ViewBase
+    {
+        public Counter()
+        {
+           //var installer = new ViewInstaller();
+        }
+    }
+}
